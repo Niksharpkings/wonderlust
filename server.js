6c11a43c0a8572211e4d790002622347e91bbf64
Niksharpkings <67552318+Niksharpkings@users.noreply.github.com>
2022-04-01 19:00:07 -0700

    fixed the image-db about and contact links in the homepage

--- server.js
+++ server.js
@@ -40,6 +40,19 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('contact', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/contact.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/about.html'));
+});
+
+app.get('/images-db', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/images-db.html'));
+});
+
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
